7cb61dc8a224d64449a0e74e41a1fe87a1f1f904
GokhanErtgn <gokhanertogan@gmail.com>
2021-08-01 12:29:39 +0300

    adding database connection informations

---
+++ Services/Catalog/CourseServices.Catalog/Settings/DatabaseSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseServices.Catalog.Settings
+{
+    public class DatabaseSettings : IDatabaseSettings
+    {
+        public string CourseCollectionName { get; set; }
+        public string CategoryCollectionName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string ConnectionString { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string DatabaseName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
--- Shared/Course.Shared/Dtos/Response.cs
+++ Shared/Course.Shared/Dtos/Response.cs
@@ -5,7 +5,7 @@ using System.Text.Json.Serialization;
 
 namespace Course.Shared.Dtos
 {
-    public class ResponseDto<T>
+    public class Response<T>
     {
         public T Data { get; set; }
 
@@ -17,19 +17,19 @@ namespace Course.Shared.Dtos
 
         public List<string> Errors { get; set; }
 
-        public static ResponseDto<T> Success(T data, int statusCode)
+        public static Response<T> Success(T data, int statusCode)
         {
-            return new ResponseDto<T> { Data = data, StatusCode = statusCode, IsSuccessfull = true };
+            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessfull = true };
         }
 
-        public static ResponseDto<T> Success(int statusCode)
+        public static Response<T> Success(int statusCode)
         {
-            return new ResponseDto<T> { Data = default(T), StatusCode = statusCode, IsSuccessfull = true };
+            return new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccessfull = true };
         }
 
-        public static ResponseDto<T> Fail(List<string> errors, int statusCode)
+        public static Response<T> Fail(List<string> errors, int statusCode)
         {
-            return new ResponseDto<T>
+            return new Response<T>
             {
                 Errors=errors,
                 StatusCode = statusCode,
@@ -37,9 +37,9 @@ namespace Course.Shared.Dtos
             };
         }
 
-        public static ResponseDto<T> Fail(string error, int statusCode)
+        public static Response<T> Fail(string error, int statusCode)
         {
-            return new ResponseDto<T>
+            return new Response<T>
             {
                 Errors = new List<string> {error},
                 StatusCode = statusCode,
