f4bef9636705a583fa5aeda3ddb85ad2bf35b2b1
GokhanErtgn <gokhanertogan@gmail.com>
2021-08-01 12:28:29 +0300

    adding mapping dtos

--- Services/Catalog/CourseServices.Catalog/Dtos/FeatureDto.cs
+++ Services/Catalog/CourseServices.Catalog/Dtos/FeatureDto.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace CourseServices.Catalog.Dtos
 {
-    public class FeatureDto
+    internal class FeatureDto
     {
         public int Duration { get; set; }
     }
