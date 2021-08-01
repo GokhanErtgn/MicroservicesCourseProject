01dbb1f7dcf19483e8157099ab71c65b4f6b0151
GokhanErtgn <gokhanertogan@gmail.com>
2021-08-01 12:30:01 +0300

    adding category service

---
+++ Services/Catalog/CourseServices.Catalog/Services/ICategoryService.cs
@@ -0,0 +1,17 @@
+﻿using Course.Shared.Dtos;
+using CourseServices.Catalog.Dtos;
+using CourseServices.Catalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseServices.Catalog.Services
+{
+    internal interface ICategoryService
+    {
+        Task<Response<List<CategoryDto>>> GetAllAsync();
+        Task<Response<CategoryDto>> GetByIdAsync(string id);
+        Task<Response<CategoryDto>> CreateAsync(Category category);
+    }
+}
