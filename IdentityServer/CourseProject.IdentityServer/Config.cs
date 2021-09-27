ec8cfd84a91bbef4a836240839a6079885c5a549
GokhanErtogan <gokhanertogan@gmail.com>
2021-09-27 19:12:13 +0300

    added basket.api

--- IdentityServer/CourseProject.IdentityServer/Config.cs
+++ IdentityServer/CourseProject.IdentityServer/Config.cs
@@ -15,7 +15,8 @@ namespace CourseProject.IdentityServer
         public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
         {
             new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
-            new ApiResource("photo_stock_catalog"){Scopes={"photo_stock_fullpermission"}},
+            new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
+            new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -33,6 +34,7 @@ namespace CourseProject.IdentityServer
             {
                 new ApiScope("catalog_fullpermission","Admin for Catalog Api"),
                 new ApiScope("photo_stock_fullpermission","Admin for Photo Stock Api"),
+                new ApiScope("basket_fullpermission","Admin for Basket Api"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -54,7 +56,7 @@ namespace CourseProject.IdentityServer
                     AllowOfflineAccess=true,
                     ClientSecrets= {new Secret("secret".Sha256())},
                     AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={ IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName, "roles"},
+                    AllowedScopes={ "basket_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName, "roles"},
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
---
+++ Services/Basket/CourseServices.Basket/Services/IBasketService.cs
@@ -0,0 +1,13 @@
+﻿using Course.Shared.Dtos;
+using CourseServices.Basket.Dtos;
+using System.Threading.Tasks;
+
+namespace CourseServices.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string userId);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> Delete(string userId);
+    }
+}
