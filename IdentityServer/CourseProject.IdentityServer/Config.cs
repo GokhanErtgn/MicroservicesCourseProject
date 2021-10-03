b3b1f417029fc1fd56156f4273921a8203578ccb
GokhanErtogan <gokhanertogan@gmail.com>
2021-10-04 00:08:29 +0300

    added order.api

--- IdentityServer/CourseProject.IdentityServer/Config.cs
+++ IdentityServer/CourseProject.IdentityServer/Config.cs
@@ -18,6 +18,7 @@ namespace CourseProject.IdentityServer
             new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+            new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
 
@@ -37,6 +38,7 @@ namespace CourseProject.IdentityServer
                 new ApiScope("photo_stock_fullpermission","Admin for Photo Stock Api"),
                 new ApiScope("basket_fullpermission","Admin for Basket Api"),
                 new ApiScope("discount_fullpermission","Admin for Discount Api"),
+                new ApiScope("order_fullpermission","Admin for Order Api"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
@@ -58,7 +60,7 @@ namespace CourseProject.IdentityServer
                     AllowOfflineAccess=true,
                     ClientSecrets= {new Secret("secret".Sha256())},
                     AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
-                    AllowedScopes={ "basket_fullpermission", "discount_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName, "roles"},
+                    AllowedScopes={ "basket_fullpermission", "discount_fullpermission", "order_fullpermission", IdentityServerConstants.StandardScopes.Email, IdentityServerConstants.StandardScopes.OpenId,IdentityServerConstants.StandardScopes.Profile,IdentityServerConstants.StandardScopes.OfflineAccess, IdentityServerConstants.LocalApi.ScopeName, "roles"},
                     AccessTokenLifetime=1*60*60,
                     RefreshTokenExpiration=TokenExpiration.Absolute,
                     AbsoluteRefreshTokenLifetime=(int)(DateTime.Now.AddDays(60)-DateTime.Now).TotalSeconds,
---
+++ Services/Order/Course.Services.Order.Application/Queries/GetOrdersByUserIdQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using Course.Services.Order.Application.Dtos;
+using Course.Services.Order.Infrastructure;
+using Course.Shared.Dtos;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Course.Services.Order.Application.Queries
+{
+    public class GetOrdersByUserIdQueryHandler : IRequestHandler<GetOrdersByUserIdQuery, Response<List<OrderDto>>>
+    {
+        private readonly OrderDbContext _context;
+
+        public GetOrdersByUserIdQueryHandler(OrderDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Response<List<OrderDto>>> Handle(GetOrdersByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var orders = await _context.Orders.Include(x => x.OrderItems).Where(x => x.BuyerId == request.UserId).ToListAsync();
+
+            if (!orders.Any())
+            {
+                return Response<List<OrderDto>>.Success(new List<OrderDto>(),200);
+            }
+
+            var ordersDto = Mapping.ObjectMapper.Mapper.Map<List<OrderDto>>(orders);
+            return Response<List<OrderDto>>.Success(ordersDto,200);
+        }
+    }
+}
